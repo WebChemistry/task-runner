c1be4cecd5f4506f0ea35f0a2c50fc59fa50ebbc
MartkCz <martk494@gmail.com>
2022-06-23 14:19:19 +0200

    downgrade StackLogger

--- src/Logger/StackLogger.php
+++ src/Logger/StackLogger.php
@@ -11,42 +11,42 @@ final class StackLogger implements LoggerInterface
 	/** @var array{mixed, string, mixed[]}[] */
 	private array $stack = [];
 
-	public function emergency(Stringable|string $message, array $context = []): void
+	public function emergency($message, array $context = []): void
 	{
 		$this->log(Severity::EMERGENCY, $message, $context);
 	}
 
-	public function alert(Stringable|string $message, array $context = []): void
+	public function alert($message, array $context = []): void
 	{
 		$this->log(Severity::ALERT, $message, $context);
 	}
 
-	public function critical(Stringable|string $message, array $context = []): void
+	public function critical($message, array $context = []): void
 	{
 		$this->log(Severity::CRITICAL, $message, $context);
 	}
 
-	public function error(Stringable|string $message, array $context = []): void
+	public function error($message, array $context = []): void
 	{
 		$this->log(Severity::ERROR, $message, $context);
 	}
 
-	public function warning(Stringable|string $message, array $context = []): void
+	public function warning($message, array $context = []): void
 	{
 		$this->log(Severity::WARNING, $message, $context);
 	}
 
-	public function notice(Stringable|string $message, array $context = []): void
+	public function notice($message, array $context = []): void
 	{
 		$this->log(Severity::NOTICE, $message, $context);
 	}
 
-	public function info(Stringable|string $message, array $context = []): void
+	public function info($message, array $context = []): void
 	{
 		$this->log(Severity::INFO, $message, $context);
 	}
 
-	public function debug(Stringable|string $message, array $context = []): void
+	public function debug($message, array $context = []): void
 	{
 		$this->log(Severity::DEBUG, $message, $context);
 	}
@@ -55,7 +55,7 @@ final class StackLogger implements LoggerInterface
 	 * @param mixed $level
 	 * @param mixed[] $context
 	 */
-	public function log($level, Stringable|string $message, array $context = []): void
+	public function log($level, $message, array $context = []): void
 	{
 		$this->stack[] = [$level, (string) $message, $context];
 	}
